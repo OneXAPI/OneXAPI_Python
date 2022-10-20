9d2c7abe8b08f0a32340bc34636788a430379113
GeaMe <gusrb145@gmail.com>
2022-10-20 15:47:07 +0900

    update cp949 error

--- OneXAPI/test/util.py
+++ OneXAPI/test/util.py
@@ -4,7 +4,7 @@ def searchLog(nowTime, searchText):
     filename = './OneXAPI_Logs/OneXAPI_' + time.strftime('%Y-%m-%d', nowTime) + '.log'
     searchTime = time.strftime('%Y-%m-%d %H:%M', nowTime)
 
-    with open(filename) as fp:
+    with open(filename, encoding='utf-8') as fp:
         logData = fp.readlines()
         for line in logData:
             if searchTime in line and searchText in line:
