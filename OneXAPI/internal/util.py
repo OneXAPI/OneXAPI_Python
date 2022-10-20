89f40991724b186ff25f8e89789a6a7a44efae7b
dhgwag <Donghoon.Gwag@gmail.com>
2022-10-20 14:03:44 +0900

    ftp download is supported

--- OneXAPI/internal/util.py
+++ OneXAPI/internal/util.py
@@ -1,24 +1,73 @@
 import ctypes, json
+import ftplib
+import os
 import site
 import platform
 from typing import Callable, Union
+from tqdm import tqdm
+
+base_path = site.getsitepackages()[0]
+file_name = 'libOneXAPI'
+
+def file_write(file, data):
+   file.write(data) 
+   global pbar
+   pbar += len(data)
+   
+def ftp_download(down_path, loc_file_name) :
+    print('Cannot found ' + loc_file_name + ' in local. Begin to download.')
+    
+    ftp=ftplib.FTP(host='27.96.134.108')
+    ftp.login()
+
+    data = []
+    #ftp.cwd("./")
+    ftp.dir(data.append)
+    
+    fd = open(down_path + loc_file_name,'wb')
+    size = ftp.size(loc_file_name)
+    pbar=tqdm(total=size)
+    
+    bufsize=1024
+    def bar(data):
+        fd.write(data)
+        pbar.update(len(data))
+
+    ftp.retrbinary("RETR " + loc_file_name, bar, bufsize)
+
+    pbar.close()
+    fd.close()
 
 if platform.system() == 'Linux' :               # Linux
+    base_path = base_path + '/OneXAPI_libs'
+    if not os.path.exists(base_path):
+        os.mkdir(base_path)
+    if not os.path.exists(base_path + '/' + file_name + '.so'):
+        ftp_download(base_path + '/', file_name + '.so')
+        
     if platform.architecture()[0] == '64bit' :
-        lib = ctypes.cdll.LoadLibrary(site.getsitepackages()[0] + '/OneXAPI_libs/libOneXAPI.so')
+        lib = ctypes.cdll.LoadLibrary(base_path + '/' + file_name + '.so')
     else :
         print("OneXAPI currently supports 64bit linux only. Please contact development team.")
         exit()
 elif platform.system() == 'Windows' :           # Windows
+    base_path = base_path + '\OneXAPI_libs'
+    if not os.path.exists(base_path):
+        os.mkdir(base_path)
+    if not os.path.exists(base_path + '\\' + file_name + '.dll'):
+        ftp_download(base_path + '\\', file_name + '.dll')
+        
     if platform.architecture()[0] == '64bit' :
-        lib = ctypes.cdll.LoadLibrary(site.getsitepackages()[0] + '/OneXAPI_libs/libOneXAPI.dll')
+        lib = ctypes.cdll.LoadLibrary(base_path + '\\' + file_name + '.dll')
     else :
         print("OneXAPI currently supports 64bit windows only. Please contact development team.")
         exit()
 elif platform.system() == 'Darwin' :            # Mac
     print("OneXAPI currently supports linux and windows only. Please contact development team.")
+    exit()
 else :
     print("We support Linux Only")
+    exit()
 
 def createObject(request: Union[str, dict], create_func: Callable):
     if type(request) is dict:
