4f61f3cb3d99ff697fa1f99b4573e2a51ee6f899
geame <gusrb145@gmail.com>
2022-10-31 14:43:26 +0900

    add replace \ -> /

--- test/util.py
+++ test/util.py
@@ -4,7 +4,7 @@ import os
 
 def searchLog(nowTime, searchText):
     path = os.path.dirname(os.path.abspath(sys.argv[0]))
-    filename = path + '/OneXAPI_Logs/OneXAPI_' + time.strftime('%Y-%m-%d', nowTime) + '.log'
+    filename = (path + '/OneXAPI_Logs/OneXAPI_' + time.strftime('%Y-%m-%d', nowTime) + '.log').replace('\\', '/')
     searchTime = time.strftime('%Y-%m-%d %H:%M', nowTime)
 
     with open(filename, encoding='utf-8') as fp:
